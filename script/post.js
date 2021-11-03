d35b62c22581229d3a11f1f372a9941a16326b0c
dabiebankz <oliviamuorah65@gmail.com>
2021-11-03 16:39:57 +0100

    final commit

--- script/post.js
+++ script/post.js
@@ -6,7 +6,7 @@ let post = localStorage.getItem("postId")
         console.log(data)
         let postLayout = document.getElementById("post-layout")
         let html = `
-        <div class="col-md-4 mb-3">
+        <div class="col-md-12 mb-3">
                  <div class="card h-100 shadow">
                         <div class="card-body">
                             <div class="d-flex justify-content-end">
